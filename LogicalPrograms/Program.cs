0a88c7829e37c66e1599c86e8fec5e5a9c3fd185
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-06 10:30:56 +0530

    Prime Number

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LogicalPrograms
+{
+    public class PrimeNumber
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Enter a value: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i =1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("Prime Number");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Not a Prime Number");
+                Console.ReadLine();
+            }
+        }
+    }
+}
